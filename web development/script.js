bd8d64713f7fc7f3cb0f27bd597dc4e3a6259a18
poudelsir <156897080+poudelsir@users.noreply.github.com>
2025-09-02 22:50:26 +0545

    Add files via upload

---
+++ web development/script.js
@@ -0,0 +1,17 @@
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+// Close modals if the user clicks outside the modal content
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+};
